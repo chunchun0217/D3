b95208fdfaeee5592f53078aa11e44036fd3f2e1
Lily <little.bear017@gmail.com>
2016-11-16 18:16:11 +0800

    js
    js

---
+++ jstest.js
@@ -0,0 +1,16 @@
+function jstest(){
+
+var width = 1024;
+var height = 1000;
+var ctrl = d3.select("body").append("svg").attr("width", width).attr("height", height);
+d3.csv("./information.csv", 
+	function(data)
+	{
+		var ln = data.length;
+		console.log(data);
+		var maxy = d3.max(data, function(d){ return d.dollar; });
+		var lines = d3.line().x(function(d,i){return i*(width/ln);}).y(function(d){return height-d.dollar*(height/maxy)});
+		ctrl.append("path").data([data]).attr("d", lines).attr("stroke", "red").attr("fill", "none");
+	}
+);
+}
